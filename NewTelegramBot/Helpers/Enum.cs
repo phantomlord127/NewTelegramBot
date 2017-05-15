4f6013b609a0d174339242d5704feeae28e4648b
Timo Brinkmann <brinkmann@softbauware.de>
2017-05-15 23:16:08 +0200

    Vorbereitung für FritzBox Steuerung

--- NewTelegramBot/Helpers/Enum.cs
+++ NewTelegramBot/Helpers/Enum.cs
@@ -2,5 +2,6 @@
 {
     None,
     MainMenu,
-    DownloadMode
+    DownloadMode,
+    ConfigRouter
 };
--- NewTelegramBot/Helpers/KeyBoard.cs
+++ NewTelegramBot/Helpers/KeyBoard.cs
@@ -13,19 +13,27 @@ namespace NewTelegramBot.Helpers
     {
         public static ReplyKeyboardMarkup GetKeyboardByTelegramBotState(TelegramBotState state)
         {
-            if (state == TelegramBotState.DownloadMode)
+            switch (state)
             {
-                return DownloadKeyboard();
+                case TelegramBotState.DownloadMode:
+                    return DownloadKeyboard();
+                case TelegramBotState.ConfigRouter:
+                    return ConfigRouter();
+                default:
+                    return MainKeyboard();
             }
-            return MainKeyboard();
         }
 
         public static ReplyKeyboardMarkup MainKeyboard()
         {
             KeyboardButton b1 = new KeyboardButton("\U0001f5a5 Server starten");
             KeyboardButton b2 = new KeyboardButton("\U0001f517 Download Modus");
-            KeyboardButton[] row = new KeyboardButton[] { b1, b2 };
-            return new ReplyKeyboardMarkup(row, true);
+            KeyboardButton b3 = new KeyboardButton("\U00002328 Router-Konfig");
+            KeyboardButton[] firstRow = new KeyboardButton[] { b1 };
+            KeyboardButton[] secondRow = new KeyboardButton[] { b2 };
+            KeyboardButton[] thirdRow = new KeyboardButton[] { b3 };
+            KeyboardButton[][] rows = new KeyboardButton[][] { firstRow, secondRow, thirdRow };
+            return new ReplyKeyboardMarkup(rows, true);
         }
 
         public static ReplyKeyboardMarkup DownloadKeyboard()
@@ -33,6 +41,17 @@ namespace NewTelegramBot.Helpers
             KeyboardButton b1 = new KeyboardButton("\U0001f519 Download Modus beenden");
             KeyboardButton[] row = new KeyboardButton[] { b1 };
             return new ReplyKeyboardMarkup(row, true);
+        }
+
+        public static ReplyKeyboardMarkup ConfigRouter()
+        {
+            KeyboardButton b1 = new KeyboardButton("\U0001f4f6 Gäste W-Lan");
+            KeyboardButton b2 = new KeyboardButton("\U0001f534 WPS toggel");
+            KeyboardButton b3 = new KeyboardButton("\U0001f519 Router-Konfig verlassen");
+            KeyboardButton[] firstRow = new KeyboardButton[] { b1, b2 };
+            KeyboardButton[] lastRow = new KeyboardButton[] { b3 };
+            KeyboardButton[][] rows = new KeyboardButton[][] { firstRow, lastRow };
+            return new ReplyKeyboardMarkup(rows, true);
         }
     }
 }
